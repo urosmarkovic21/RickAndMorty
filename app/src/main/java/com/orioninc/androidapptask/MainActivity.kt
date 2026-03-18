7c153bca1153450648b5256225d3fa289300cda0
Uroš Marković <uros.markovic@mera.com>
2026-03-18 16:27:26 +0100

    added splash

--- app/src/main/java/com/orioninc/androidapptask/MainActivity.kt
+++ app/src/main/java/com/orioninc/androidapptask/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.orioninc.androidapptask.ui.navigation.AppNavGraph
 import com.orioninc.androidapptask.ui.theme.AndroidAppTaskTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen().setOnExitAnimationListener { splashScreenView ->
+            splashScreenView.view.animate()
+                .alpha(0f)
+                .setDuration(5000L)
+                .withEndAction {
+                    splashScreenView.remove()
+                }
+                .start()
+        }
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             AndroidAppTaskTheme {
                 AppNavGraph()
